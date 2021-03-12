48a997d941d020b307acaf3a248bb6e77f8a1176
shuihuo <cqdxwjd@163.com>
2021-03-12 11:49:10 +0800

    test gitignore file

---
+++ src/main/java/cqdxwjd/flink/batch/BatchWordCountJava.java
@@ -0,0 +1,11 @@
+package cqdxwjd.flink.batch;
+
+/**
+ * 单词计数之离线计算
+ */
+public class BatchWordCountJava {
+    public static void main(String[] args) {
+        String inputPath = "C:\\Users\\shuihuo\\Desktop\\git\\flink-in-action\\data\\file";
+
+    }
+}
---
+++ src/main/scala/cqdxwjd/flink/streaming/SocketWindowWordCountScala.scala
@@ -0,0 +1,35 @@
+package cqdxwjd.flink.streaming
+
+import org.apache.flink.api.java.utils.ParameterTool
+import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
+import org.apache.flink.streaming.api.windowing.time.Time
+
+/**
+ * 单词计数之滑动窗口计算
+ */
+object SocketWindowWordCountScala {
+  def main(args: Array[String]): Unit = {
+    // 获取Socket端口号
+    val port: Int = try {
+      ParameterTool.fromArgs(args).getInt("port")
+    } catch {
+      case e: Exception => {
+        System.err.println("No port set. use default port 9999--Scala")
+        9999
+      }
+    }
+    // 获取运行环境
+    val env: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
+    // 连接Socket获取输入数据
+    val text = env.socketTextStream("172.25.5.11", port, '\n')
+    // 解析数据
+    import org.apache.flink.api.scala._
+    val windowCounts = text.flatMap(line => line.split("\\s")).map(w => WordWithCount(w, 1L)).keyBy("word").timeWindow(Time.seconds(2),
+      Time.seconds(1)).sum("count")
+    windowCounts.print().setParallelism(1)
+    // 执行任务
+    env.execute("Socket window count")
+  }
+}
+
+case class WordWithCount(word: String, count: Long)
